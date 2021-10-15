62f9cfb04c8b55f4cc687bf3fe30da92befe542d
sunisakc <sunisa.kan@gmail.com>
2021-10-15 23:20:28 +0200

    Add Model

--- FilmList_MVC/Controllers/HomeController.cs
+++ FilmList_MVC/Controllers/HomeController.cs
@@ -27,7 +27,13 @@ namespace FilmList_MVC.Controllers
         {
             return View();
         }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
 
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
