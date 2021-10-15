e09c5db81965ef55f37991938d486d97ebe97646
sunisakc <sunisa.kan@gmail.com>
2021-10-15 22:05:16 +0200

    Create Controller and View

---
+++ FilmList_MVC/Controllers/Movie.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmList_MVC.Controllers
+{
+    public class Movie : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
